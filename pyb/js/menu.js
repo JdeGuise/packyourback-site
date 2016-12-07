67f82d4b488f528c759656795188d5fe05f19aa5
Jon Ragon <jonragon@AUX-db6da5.gha.chartermi.net>
2016-12-06 19:13:17 -0500

    Fixes #17; adds updated hamburger menu to all html pages

--- pyb/js/menu.js
+++ pyb/js/menu.js
@@ -1,24 +1,8 @@
-// var thing1 = document.getElementById("hamburger");
-// thing1.onclick = function() {
-// 	var origclassname = "dropdown_menuitem"
-// 	//alert("test")
-// 	//var str = thing1.className;
-// 	alert(String(thing1.className));
-// 	if(!thing1.className.includes('show'){
-// 		document.getElementById("hamburger").classList = origclassname + " " + "show";
 
-// 	} 
-// 	else {
-// 		thing1.classList = origclassname + " " + "hide";
-// 	}
-// }
 function testf(){
 	alert("test");
 }
 $(document).ready( function() {
-
-
-
 	$('#hamburger').on('click', function(){ 
 		var dropdown = document.getElementById("myDropdown");
 		var dropdown_overlay = document.getElementById("dropdownOverlay");
@@ -26,35 +10,35 @@ $(document).ready( function() {
 
 
 		if (display == "block"){
+			// dropdown.css("display","none")
+			// dropdown_overlay.css({"display":"none"})
 			dropdown.style.display= "none";
 			dropdown_overlay.style.display = "none";
+			$(this).css({"transform" : "rotate(12deg)", "opacity" : ".8"});
 		}
 		else {
 			dropdown.style.display = "block";
 			dropdown_overlay.style.display = "block";
+			
+			$(this).css({"transform" : "rotate(-90deg)", "opacity" : ".8"});
 		}
 	});
 });
 
 
-// var $modalOverlay = $('#dropdownOverlay');
-// var $modal        = $('#thedropdown');
-// var $modalClose   = $('#hamburger');
-// var $modalOpen    = $('#hamburger');
 
-// $modalOpen.click(function(){
-//   $modalOverlay.stop().fadeTo(500,1);
-// });
+$("#hamburger").hover(
+  function(){
+  	if (document.getElementById("myDropdown").style.display == "block"){
+    	$(this).css({"opacity" : ".8", "transform" : "rotate(-90deg)","transition" : ".3s ease"});
+	} else {
+		$(this).css({"opacity" : ".8", "transform" : "rotate(12deg)", "transition" : ".3s ease"});
+	}
+  }, function(){
+  	if (document.getElementById("myDropdown").style.display == "block"){
+    	$(this).css({"opacity" : "1", "transform" : "rotate(-90deg)"});
+    } else {
+    	$(this).css({"opacity" : "1", "transform" : "rotate(0deg)"});
+    }
+  });
 
-// $modalOverlay.click(function(){
-//   $modalOverlay.stop().fadeTo(500,0, function(){ $(this).hide(); });
-// });
-
-
-// $modal.click(function( e ) {
-//    e.stopPropagation(); 
-// });
-
-// $modalClose.click(function(){
-//   $modalOverlay.click();
-// });
