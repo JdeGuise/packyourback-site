0ac5d64a9ce53bda2b1f7430b677e39681772802
cyropulse <cyropulse@gmail.com>
2017-03-10 20:31:30 -0500

    addressed #44s issues and added in adjustments based on window sizes

--- public_html/html/ourcause.php
+++ public_html/html/ourcause.php
@@ -78,28 +78,30 @@
   <br>
   <br>
           <div class="columns small-12 medium-12 large-12">
-            <a href="https://www.cmich.edu/stamp/Pages/pack-your-back.aspx"><h3>Pack Your Back at CMU</h3></a>
+            <a href="https://www.cmich.edu/stamp/Pages/pack-your-back.aspx"><h1>Pack Your Back at CMU</h1></a>
           </div>
         </div>
 
       <div class="row">
 
-          <div class="columns small-12 medium-8 large-5" >
+          <div class="columns small-12 medium-8 large-5" style="left:80px; top:65px;" >
                 <br>
                 <br>
+                <h4 style="margin-left:25px;">How did Pack Your Back start?</h4>
                 <br>
-                  <p style="text-align:left; margin-left: 25px; max-width: 800px;">Pack Your Back is a nonprofit organization that was started in a dorm room in Calkins Hall. It’s a few students at CMU who decided that they wanted to be hands on, they wanted to be proactive in helping the younger generation succeed. With that, Pack Your Back was born.</p>
-                  <br>                         <p style="text-align:left; margin-left: 25px; max-width: 800px;">In January 2016, Galen Miller was volunteering in Flint with his fraternity. As they continued to deliver water and other supplies to the residents suffering from the water crisis, Miller noticed that as the major media outlets began to leave, so did the help.</p>
+                <br>
+                  <p style="text-align:left; margin-left: 25px; max-width: 85%;">Pack Your Back is a nonprofit organization that was started in a dorm room in Calkins Hall. It’s a few students at CMU who decided that they wanted to be hands on, they wanted to be proactive in helping the younger generation succeed. With that, Pack Your Back was born.</p>
+                  <br>                         <p style="text-align:left; margin-left: 25px; max-width: 85%;">In January 2016, Galen Miller was volunteering in Flint with his fraternity. As they continued to deliver water and other supplies to the residents suffering from the water crisis, Miller noticed that as the major media outlets began to leave, so did the help.</p>
                   <br>
-                <p style="text-align:left; margin-left: 25px; max-width: 800px;">
+                <p style="text-align:left; margin-left: 25px; max-width: 90%;">
                     That night after the event in Flint, Miller got together a group of friends in his dorm room, and they discussed how they could continue to give back to the community in Flint. They settled on collecting and donating school supplies, as they realized that the water crisis had a massive impact on the development of young school-aged children.
                     </p>
 
-              <p style="text-align:left; margin-left: 25px; max-width: 800px;">
+              <p style="text-align:left; margin-left: 25px; max-width: 90%;">
 
 A group of CMU students and alumni started Pack Your Back in March 2016 and were officially approved as a 501(c)(3) nonprofit in May. They set to work collecting funds and school supplies to donate to the children of Flint and beyond.
               </p>
-              <p style="text-align:left; margin-left: 25px; max-width: 800px;">
+              <p style="text-align:left; margin-left: 25px; max-width: 90%;">
 The organization plans to extend to the rest of the state and even the country in the following months and years and already has an expansion plan in place.
               </p>
 
@@ -109,7 +111,7 @@ The organization plans to extend to the rest of the state and even the country i
           <div class="columns small-12 medium-4 large-7">
                 <br>
                 <br>
-                <img src="../assets/ourcause/calkins.jpg" width="80%" style="margin-left:30px;" />
+                <img src="../assets/ourcause/calkins.jpg" width="60%" style="left:235px; position: relative;" id="calkinsphoto"/>
           </div>
 
       </div>
