5832d6e47d18a1fd35316610079a7e7620f0dd07
John deGuise <jrd120893@gmail.com>
2017-06-26 20:26:03 -0400

    Work on #7;  refactors <head> code into new file, refactors <header> code into new file, adds changes to index.php, and code organization cleanup on index.php

---
+++ public_html/html/includes/header.php
@@ -0,0 +1,50 @@
+		<header class="header">
+  <!-- columns and small-12 are Foundation classes that split the browser window into 12 columns
+	  that can actively resize themselves -->
+			<div class="columns small-12">
+
+				<ul class="topnav" id="myTopNav">
+					<a href="index.php"><img id="logo" src="assets/pyb_row_270x70px.png"></a>
+					<li class="menuitem" onclick="location.href='index.php';">
+						<a href="index.php">Home</a></li>
+					<li class="menuitem" onclick="location.href='php/blogmodule/index.php';"><a href="php/blogmodule/index.php">Blog</a></li>					<li class="menuitem" onclick="location.href='html/leadership.php';">
+						<a href="html/leadership.php">Leadership</a></li>
+					<li class="menuitem" onclick="location.href='html/ourcause.php';">
+						<a href="html/ourcause.php">Our Cause</a></li>
+					<li class="menuitem" onclick="location.href='html/getinvolved.php';">
+						<a href="html/getinvolved.php">Get Involved</a></li>
+						<li class="menuitem">
+							<button class="donatebuttons" onclick="location.href='https://www.classy.org/campaign/Pack-Your-Back/c128367'" style="background-color:crimson;">FUNDRAISERS</button>
+						</li>
+		            <li class="menuitem">
+		            <button class="donatebuttons" onclick="location.href='https://www.paypal.me/PackYourBack'" style="background-color:crimson;">DONATE</button></li>
+					<ul>
+						<img id="hamburger" src="assets/hamburgermenu-blue.png">
+					</ul>
+				</ul>
+			</div>
+		</header>
+
+		<div id="dropdownOverlay">
+			<div class="thedropdown">
+				<ul class="dropdown" id="myDropdown" >
+
+				<br>
+					<li class="dropdown_menuitem" onclick="location.href='index.php';">
+						<a href="index.php">Home</a>
+					</li>
+ 						<li class="dropdown_menuitem" onclick="location.href='php/blogmodule/index.php';"><a href="php/blogmodule/index.php">Blog</a></li>					<li class="dropdown_menuitem" onclick="location.href='html/leadership.php';">
+						<a href="html/leadership.php">Leadership</a>
+					</li>
+					<li class="dropdown_menuitem"onclick="location.href='html/ourcause.php';">
+						<a href="html/ourcause.php">Our Cause</a>
+					</li>
+					<li class="dropdown_menuitem" onclick="location.href='html/getinvolved.php';">
+						<a href="html/getinvolved.php">Get Involved</a>
+					</li>
+					<li>
+						<button id="dropdown_donatebutton" onclick="location.href='https://www.classy.org/campaign/Pack-Your-Back/c128367'" style="background-color:crimson;">Donate</button>
+					</li>
+				</ul>
+			</div>
+		</div>
--- public_html/index.php
+++ public_html/index.php
@@ -1,284 +1,231 @@
 <!DOCTYPE html>
 <html>
   <head>
+	
 	<title>Pack Your Back - Supplying Success</title>
 
-	<meta charset="utf-8"/>
-	<meta name="viewport" content="initial-scale=1.0">
-	<meta name="google-site-verification" content="qt_-l__afKRIRexX94lbqPj8nf87AolmgIn0vJMInSc" />
+	<?php include('html/includes/head.php'); ?>
 
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-	<link rel="stylesheet" href="css/foundation.css" />
-	<link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Raleway|Roboto|Source+Sans+Pro" rel="stylesheet">
-
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script>
-	<script src="http://malsup.github.com/jquery.form.js"></script>
-	<link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico" />
   </head>
-	<body class="body">
-		<header class="header">
-  <!-- columns and small-12 are Foundation classes that split the browser window into 12 columns
-	  that can actively resize themselves -->
+
+  <body class="body">
+  	<?php include('html/includes/header.php'); ?>
+
+  	<br>
+	<br>
+	<br>
+
+	<div class="slideshow-container" style="background-color:rgb(242,242,245);">
+	  	<div class="mySlides fade" >
+	  		<a href="https://packyourback.regfox.com/jogyourdog"><img src="assets/index_pics/rte_p1.png" id="jumboimg"/></a>
+	  	</div>
+		<div class="mySlides fade">
+			<a href="https://docs.google.com/forms/d/e/1FAIpQLSdh6jzy5NdV1VhpOp-151iJmgI6UsDZtayLunLfOlTlUs9-NQ/viewform"><img src="assets/index_pics/cco_p2.png" id="jumboimg"></a>
+		</div>
+		<div class="mySlides fade">
+			<a href="http://packyourback.org/html/leadership.php"><img src="assets/index_pics/ldr_p3.png" id="jumboimg"></a>
+		</div>
+		<div class="mySlideDots" style="position: relative; top: -35px; text-align:center">
+			<span class="dot"></span>
+			<span class="dot"></span>
+			<span class="dot"></span>
+		</div>
+	</div>
+
+	<div class="donaterow row" style="width:95%; max-width: 95%;">
+
+		<div class="columns small-12 medium-6 large-8" id="why_donate_content">
+		
+			<br>
+			<h1>Why Donate?</h1>
+			<br>
+			<br>
+			
+			<p>Pack Your Back relies on the kind donations from the community and various businesses in order to provide resources for children to succeed in the classroom. Since there are always more children in need, Pack Your Back is always in need of donations to continue providing students with resources.</p>
+
+			<br>
+			<br>
+			<p>In addition to donations of backpacks and other school supplies, the Pack Your Back movement is relying on sponsorships from local businesses and organizations to fund the cause. Click the link below for more information.</p>
+			<br>
+			<br>
+			
+			<div class="columns small-6" style="text-align:center;">
+				<a href="https://www.classy.org/campaign/Pack-Your-Back/c128367"><button>Donate Now</button></a>
+			</div>
+			<div class="columns small-6" style="text-align:center;">
+				<a href="https://www.classy.org/campaign/Pack-Your-Back/c128367"><button>More Info</button></a>
+			</div>
+
+
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+
+		</div>
+		
+		<div class="columns small-12 medium-6 large-4">
+
 			<div class="columns small-12">
 
-				<ul class="topnav" id="myTopNav">
-					<a href="index.php"><img id="logo" src="assets/pyb_row_270x70px.png"></a>
-					<li class="menuitem" onclick="location.href='index.php';">
-						<a href="index.php">Home</a></li>
-					<li class="menuitem" onclick="location.href='php/blogmodule/index.php';"><a href="php/blogmodule/index.php">Blog</a></li>					<li class="menuitem" onclick="location.href='html/leadership.php';">
-						<a href="html/leadership.php">Leadership</a></li>
-					<li class="menuitem" onclick="location.href='html/ourcause.php';">
-						<a href="html/ourcause.php">Our Cause</a></li>
-					<li class="menuitem" onclick="location.href='html/getinvolved.php';">
-						<a href="html/getinvolved.php">Get Involved</a></li>
-						<li class="menuitem">
-							<button class="donatebuttons" onclick="location.href='https://www.classy.org/campaign/Pack-Your-Back/c128367'" style="background-color:crimson;">FUNDRAISERS</button>
-						</li>
-		            <li class="menuitem">
-		            <button class="donatebuttons" onclick="location.href='https://www.paypal.me/PackYourBack'" style="background-color:crimson;">DONATE</button></li>
-					<ul>
-						<img id="hamburger" src="assets/hamburgermenu-blue.png">
-					</ul>
-				</ul>
-			</div>
-		</header>
-
-		<div id="dropdownOverlay">
-			<div class="thedropdown">
-				<ul class="dropdown" id="myDropdown" >
-
+				<img src="assets/index_pics/kids.png" style="width:494px; height:240px; position:relative;">
 				<br>
-					<li class="dropdown_menuitem" onclick="location.href='index.php';">
-						<a href="index.php">Home</a>
-					</li>
- 						<li class="dropdown_menuitem" onclick="location.href='php/blogmodule/index.php';"><a href="php/blogmodule/index.php">Blog</a></li>					<li class="dropdown_menuitem" onclick="location.href='html/leadership.php';">
-						<a href="html/leadership.php">Leadership</a>
-					</li>
-					<li class="dropdown_menuitem"onclick="location.href='html/ourcause.php';">
-						<a href="html/ourcause.php">Our Cause</a>
-					</li>
-					<li class="dropdown_menuitem" onclick="location.href='html/getinvolved.php';">
-						<a href="html/getinvolved.php">Get Involved</a>
-					</li>
-					<li>
-						<button id="dropdown_donatebutton" onclick="location.href='https://www.classy.org/campaign/Pack-Your-Back/c128367'" style="background-color:crimson;">Donate</button>
-					</li>
-				</ul>
 			</div>
+			
+			<div class="columns small-12">
+				
+				<br>
+				<img src="assets/index_pics/1.jpeg" style="height:240px; position: relative;">
+			</div>
+
+		</div>
+	</div>
+
+
+	<div class="row">
+		
+		<br>
+	
+		<hr>
+	
+		<br>
+		
+		<div class="columns small-12 medium-4 large-4 story_feature">
+	
+			<br>
+			<div class="story_circles">
+				<a href="html/getinvolved.php">
+					<img src='assets/story_pics/supplies.png'>
+				</a>
+			</div>
+			<br>
+			
+			<h2>Our Needs</h2>
+			<p class="story_p">Pack Your Back relies on the charitable donations from the community members to help supply students with resources for the classroom.</p>
+			
+			<br>
+			<br>
 		</div>
 
-		<br>
-		<br>
-		<br>
-		<div class="slideshow-container" style="background-color:rgb(242,242,245);">
-			  <div class="mySlides fade" >
-						<a href="https://packyourback.regfox.com/jogyourdog"><img src="assets/index_pics/rte_p1.png" id="jumboimg"/></a>
-			  </div>
-			  <div class="mySlides fade">
-					<a href="https://docs.google.com/forms/d/e/1FAIpQLSdh6jzy5NdV1VhpOp-151iJmgI6UsDZtayLunLfOlTlUs9-NQ/viewform"><img src="assets/index_pics/cco_p2.png" id="jumboimg"></a>
-			  </div>
-			  <div class="mySlides fade">
-					<a href="http://packyourback.org/html/leadership.php"><img src="assets/index_pics/ldr_p3.png" id="jumboimg"></a>
-			  </div>
-				<div class="mySlideDots" style="position: relative; top: -35px; text-align:center">
-				  <span class="dot"></span>
-				  <span class="dot"></span>
-				  <span class="dot"></span>
-				</div>
-		</div>
+		<div class="columns small-12 medium-4 large-4 story_feature">
 
-<!-- <div class="row blogrow">
-				<h2 style="padding-top:2%;">Current Events</h2>
-				<br>
-				<div class="columns large-12">
-					<div class="columns small-12 medium-6 large-6 blogbox">
-						<h3>Race to Educate by Pack Your Back</h3>
-						<p>Pack Your Back presents Race to Educate, a fun run to raise money to fulfill our mission: providing students in need with backpacks and school supplies. We need your help in order to support this amazing cause in the Detroit area this summer.</p>
-						<p>Participants will have the opportunity to join us for a morning of FUN at Eastwood Beach, which is located at Stony Creek Metropark in Shelby Township, Michigan. We are also welcoming dogs at the event (must be on a leash).</p>
-						<br>
-						<br>
-					<div class="columns small-6 medium-6 large-6">
-						<button onclick="location.href='https://packyourback.regfox.com/racetoeducate'">Register Now</button>						
-					</div>					
-					<div class="columns small-6 medium-6 large-6">
-						<button onclick="location.href='https://www.google.com/maps/place/4300+Main+Park+Dr,+Shelby+Charter+Township,+MI+48316/@42.7105564,-83.0752789,16z/data=!4m5!3m4!1s0x8824e62278d72421:0xd51b7cba39e8c0e7!8m2!3d42.7110373!4d-83.0715023'">Directions</button>						
-					</div>
-					</div>
-					<div class="columns small-12 medium-6 large-6 blogbox" style="text-align: center;">
-						<img src="assets/blog/ce/rte.png" width="60%" style="position: relative; top:20px;">
-					</div>
-				</div>				
-
+			<br>
+			<div class="story_circles">
+				<a href="html/leadership.php">
+					<img src='assets/story_pics/swinging.png'>
+				</a>
 			</div>
-				<hr>
+			<br>
+			
+			<h2>Our Leaders</h2>
+			<p class="story_p">Pack Your Back is proud to be led by a young generation, and we believe it's our responsibility to build a system for helping younger generations receive the education they need.</p>
+			
+			<br>
+			<br>
+		</div>
+		<div class="columns small-12 medium-4 large-4 story_feature">
+
+			<br>
+			<div class="story_circles">
+				<a href="https://www.cmich.edu/stamp/Pages/pack-your-back.aspx">
+					<img src='assets/story_pics/paints.png'>
+				</a>
+			</div>
+						
+			<br>
+			<h2>Our Story</h2>
+			<p class="story_p">Working for the benefit of the underprivileged in the public education system through distribution events and community get-togethers, helping to give students opportunities to interact and grow.</p>
+			<br>
+			<br>
+
+		</div>
+	</div>
+
+	<div id="index-quote-wrapper">
+		<div class="row" id="index-quote">
+	
+			<br>
+			<br>
+		
+			<div id="index-quote-self">
+				<h4>23% of Michigan residents fall below the poverty line.  There is a constant need for education supplies to help children succeed.</h4>
+			</div>
+			
+			<br>
+			<br>
+
+		</div>
+	</div>
 
 
-<br>
-			 -->	<div class="donaterow row" style="width:95%; max-width: 95%;">
+	<br>
+	<br>
 
-					<div class="columns small-12 medium-6 large-8" id="why_donate_content">
-								<br>
-							  <h1>Why Donate?</h1>
-							  <br>
-							  <br>
-								<p>
-								  Pack Your Back relies on the kind donations from the community and various businesses in order to provide resources for children to succeed in the classroom. Since there are always more children in need, Pack Your Back is always in need of donations to continue providing students with resources.
-								</p>
+	<h3 style="text-align: center;">Keep up with Pack Your Back</h3>
 
-								<br>
-								<br>
-								<p>
-								  In addition to donations of backpacks and other school supplies, the Pack Your Back movement is relying on sponsorships from local businesses and organizations to fund the cause. Click the link below for more information.
-								</p>
-								<br>
-								<br>
-								<div class="columns small-6" style="text-align:center;">
-									<a href="https://www.classy.org/campaign/Pack-Your-Back/c128367"><button>Donate Now</button></a>
-								</div>
-								<div class="columns small-6" style="text-align:center;">
-									<a href="https://www.classy.org/campaign/Pack-Your-Back/c128367"><button>More Info</button></a>
-								</div>
+	<br>
+	<br>
 
-
-									<br>
-									<br>
-									<br>
-									<br>
-									<br>
-
-					</div>
-					<div class="columns small-12 medium-6 large-4">
-
-						<div class="columns small-12">
-
-							<img src="assets/index_pics/kids.png" style="width:494px; height:240px; position:relative;">
-							<br>
-						</div>
-						<div class="columns small-12">
-							<br>
-							<img src="assets/index_pics/1.jpeg" style="height:240px; position: relative;">
-
-						</div>
-
-
-					</div>
-
-				</div>
-
-
-				<div class="row">
+	<div class="row">
+		<div class="columns small-6 medium-3 large-3">
+			<div class="sm_circles">
+				<a href="html/contact.php">
+					<img id="social_media_icons" src="assets/social_media/email-icon.png">
 					<br>
-	<hr>
-									<br>
-					<div class="columns small-12 medium-4 large-4 story_feature">
-						<br>
+					<h6 id="social_media_text">Send us an<br>Email</h6>
+				</a>
+			</div>
+			
+			<br>
+		</div>
 
-						<div class="story_circles">
-						  <a href="html/getinvolved.php">
-							<img src='assets/story_pics/supplies.png'>
-						  </a>
-						</div>
-						<br/>
-						<h2>Our Needs</h2>
-						<p class="story_p">Pack Your Back relies on the charitable donations from the community members to help supply students with resources for the classroom.</p>
-						<br>
-						<br>
-					</div>
+		<div class="columns small-6 medium-3 large-3">
+			<div class="sm_circles">
+				<a href="https://twitter.com/PackYourBack">
+					<img id="social_media_icons" src="assets/social_media/circle-twitter-128.png">
+					<br>
+					<h6 id="social_media_text">Follow us on Twitter</h6>
+				</a>
+			</div>
+			
+			<br>
+		</div>
 
-					<div class="columns small-12 medium-4 large-4 story_feature">
-
-									<br>
-						<div class="story_circles">
-						  <a href="html/leadership.php">
-							<img src='assets/story_pics/swinging.png'>
-						  </a>
-						</div>
-							<br/>
-						<h2>Our Leaders</h2>
-						<p class="story_p">Pack Your Back is proud to be led by a young generation, and we believe it's our responsibility to build a system for helping younger generations receive the education they need.</p>
-						<br>
-						<br>
-					</div>
-					<div class="columns small-12 medium-4 large-4 story_feature">
-
-									<br>
-						<div class="story_circles">
-						  <a href="https://www.cmich.edu/stamp/Pages/pack-your-back.aspx">
-							<img src='assets/story_pics/paints.png'>
-						  </a>
-						</div>
-						<br/>
-						<h2>Our Story</h2>
-						<p class="story_p">Working for the benefit of the underprivileged in the public education system through distribution events and community get-togethers, helping to give students opportunities to interact and grow.</p>
-						<br>
-						<br>
-					</div>
-				</div>
-
-				<div id="index-quote-wrapper">
-					<div class="row" id="index-quote">
-						<br/>
-						<br/>
-						<div id="index-quote-self">
-							<h4>23% of Michigan residents fall below the poverty line.  There is a constant need for education supplies to help children succeed.</h4>
-						</div>
-						<br>
-					  <br/>
-					</div>
-				</div>
-
-
-<br>
-<br>
-
-			<h3 style="text-align: center;">Keep up with Pack Your Back</h3>
-
-
-<br>
-<br>
-	  <div class="row">
-			<div class="columns small-6 medium-3 large-3">
-			  <div class="sm_circles">
-				<a href="html/contact.php"><img id="social_media_icons"  src="assets/social_media/email-icon.png"><br>
-				<h6 id="social_media_text">Send us an<br>Email</h6></a>
+		<div class="columns small-6 medium-3 large-3">
+			<div class="sm_circles">
+				<a href="https://www.facebook.com/PackYourBack">
+					<img id="social_media_icons" src="assets/social_media/circle-facebook-128.png">
+					<br>
+					<h6 id="social_media_text">Like us on Facebook</h6>
+				</a>
 			  </div>
+
 			  <br>
 			</div>
 
 			<div class="columns small-6 medium-3 large-3">
-			  <div class="sm_circles">
-				<a href="https://twitter.com/PackYourBack"><img id="social_media_icons"  src="assets/social_media/circle-twitter-128.png"><br>
-				<h6 id="social_media_text">Follow us on Twitter</h6></a>
-			  </div>
-			  <br>
+				<div class="sm_circles">
+					<a href="https://www.linkedin.com/company/pack-your-back">
+						<img id="social_media_icons" src="assets/social_media/circle-linkedin-128.png">
+						<br>
+						<h6 id="social_media_text">Follow us on LinkedIn</h6>
+					</a>
+			  	</div>
+
+			  	<br>
 			</div>
+	</div>
 
-			<div class="columns small-6 medium-3 large-3">
-			  <div class="sm_circles">
-				<a href="https://www.facebook.com/PackYourBack"><img id="social_media_icons"  src="assets/social_media/circle-facebook-128.png"><br>
-				<h6 id="social_media_text">Like us on Facebook</h6></a>
-			  </div>
-			  <br>
-			</div>
+	<br>
 
-			<div class="columns small-6 medium-3 large-3">
-			  <div class="sm_circles">
-				<a href="https://www.linkedin.com/company/pack-your-back"><img id="social_media_icons"  src="assets/social_media/circle-linkedin-128.png"><br>
-				<h6 id="social_media_text">Follow us on LinkedIn</h6></a>
-			  </div>
-			  <br>
-			</div>
-	  </div>
-
-
-			<br/>
 	<!-- Footer Section -->
 
 	<?php include("html/includes/footer.php") ?>
 
-	<!-- close footer content -->
-	  </body><!-- close body -->
-	 <script>
+	</body>
+
+	<script>
 
 			// This function controls the paneling and transition of our slide show.
 			//	Without it, the pictures will all display in a column and the transition will break
@@ -312,38 +259,12 @@
 				// change image every 10 seconds
 				setTimeout(showSlides, 5000);
 			}
+
 			//start the slide index at 0
 			var slideIndex = 0;
+
 			//run our function
 			showSlides();
-
-			// function testf(){
-			// 	alert("test");
-			// }
-			// function toggleMenu(){
-			// 	var dropdown = document.getElementById("myDropdown")
-			// 	var display = dropdown.style.display;
-
-
-			// 	if (display == "none"){
-			// 		dropdown.style.display = "block"
-			// 	}
-			// 	else {
-			// 		dropdown.style.display = "none"
-			// 	}
-			// }
 	 </script>
-	<script>
-		// wait for the DOM to be loaded
-		// $(document).ready(function() {
-		// 	//bind 'subscribe-form' and provide a simple callback function
-
-		// 	$('#subscribe-form').on(function() {
-		// 	    alert("Thank you!");
-		// 	});
-		// });
-	</script>
 	 <script src="js/menu.js" ></script>
 	 <script src="js/social_media_animation.js"></script>
-
-</html>
