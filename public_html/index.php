0106c824c40076b450461e8aae216585991bf279
John deGuise <jrd120893@gmail.com>
2018-09-14 09:59:09 -0600

    Refactor css

--- public_html/index.php
+++ public_html/index.php
@@ -77,28 +77,32 @@
 			<br>
 			<br>
 
-			<iframe class="distribution_map2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2948.248518823795!2d-83.06880814851169!3d42.35854267908491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824d2bcfe73c635%3A0xc56df4d96dd2790a!2s5201+Woodward+Ave%2C+Detroit%2C+MI+48202!5e0!3m2!1sen!2sus!4v1498781104910" frameborder="0" style="border: 1px solid rgb(55,55,55); bottom: 45px; position: relative;text-align: center;" allowfullscreen></iframe>
+			<iframe class="distribution_map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2948.248518823795!2d-83.06880814851169!3d42.35854267908491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824d2bcfe73c635%3A0xc56df4d96dd2790a!2s5201+Woodward+Ave%2C+Detroit%2C+MI+48202!5e0!3m2!1sen!2sus!4v1498781104910" frameborder="0" allowfullscreen></iframe>
 		</div>
 		<hr class="hrstyle">
 		<div class="columns small-12 medium-6 large-6">
 			
 			<br>
 			
-			<h3 style="text-align: center;">Little Miss Flint</h3>
+			<h3>Little Miss Flint</h3>
 			
 			<br>
 			
-			<p style="padding-left: 35px; position: relative; text-align: left;">Pack Your Back would like to thank Mari Copeny, also known as "Little Miss Flint” for assisting our organization in raising funds to purchase over 1,000 for students in Flint, Michigan. "We need to help kids in Flint that can't afford a backpack or school supplies so the people can go to school," she said. "School is important. Education is life.” - Mari Copeny</p>
+			<p>
+				Pack Your Back would like to thank Mari Copeny, also known as "Little Miss Flint” for assisting our organization in raising funds to purchase over 1,000 for students in Flint, Michigan. "We need to help kids in Flint that can't afford a backpack or school supplies so the people can go to school," she said. "School is important. Education is life.” - Mari Copeny
+			</p>
 			
 			<br>
 			
-			<p style="left: 35px; position: relative; text-align: left;">To donate to Mari’s 2018 Pack Your Back campaign, please visit her fundraising page, located <a href="https://www.classy.org/team/129320">here</a>.</p>
+			<p>
+				To donate to Mari’s 2018 Pack Your Back campaign, please visit her fundraising page, located <a href="https://www.classy.org/team/129320">here</a>.
+			</p>
 			
 			<br>
 
 		</div>
-		<div class="columns small-12 medium-6 large-6" style="text-align:center;">
-			<img src="assets/index_pics/lmf.png" width="80%" height="80%" style="border: 1px solid black;">
+		<div class="columns small-12 medium-6 large-6">
+			<img src="assets/index_pics/lmf.png" width="80%" height="80%">
 			
 			<br>
 			<br>
@@ -110,39 +114,55 @@
 
 	</div>
 
-	<div class="row" style="border: 3px solid rgb(111,111,111); margin: 0 auto;">
-		<div class="columns small-12 medium-12 large-12" style="margin-bottom: 15px; width: 100%;">
+	<div class="row" id="sponsor-container">
+		<div class="columns small-12 medium-12 large-12">
 
 			<br>
 			<br>
 
-			<h4 style="text-align: center;"><b>Our sincerest thanks to our 2017/2018 organizational sponsors.</h4>
+			<h4>
+				<b>Our sincerest thanks to our 2017/2018 organizational sponsors.</b>
+			</h4>
 
 			<br>
 			<br>
 
-			<section class="sponsorship_container center slider" style="width: 85%; margin: 0 auto;">
-				<a href="https://www.concentra.com/"><div class="small-12" style="width: 100%;">
-					<img src="assets/logos/concentra.png" style="width: auto;">
-				</div></a>
-				<a href="http://www.cmufa.com/"><div class="small-12">
-					<img src="assets/logos/cmufa_logo.png" style="width: auto;">
-				</div></a>
-				<a href="http://www.brightsidedental.com/"><div class="small-12">
-					<img src="assets/logos/brightsidedental.png" style="width: auto;">
-				</div></a>
-				<a href="http://www.detroitpubliclibrary.org/"><div class="small-12">
-				    <img src="assets/logos/dpl.png" style="width: auto;">
-				</div></a>
-				<a href="http://www.mcc.edu/"><div class="small-12">
-					<img src="assets/logos/mottcc.png" style="width: 85%;">
-				</div></a>
-				<a href="http://www.nthconsultants.com/"><div class="small-12">
-					<img src="assets/logos/nth_consultants.png" style="width: auto;">
-				</div></a>
-				<a href="http://www.masco.com"><div class="small-12">
-					<img src="assets/logos/masco.png" style="width: 95%;">
-				</div></a>
+			<section class="sponsorship-container-section center slider">
+				<a href="https://www.concentra.com/">
+					<div class="small-12" id="concentra-logo">
+						<img src="assets/logos/concentra.png">
+					</div>
+				</a>
+				<a href="http://www.cmufa.com/">
+					<div class="small-12">
+						<img src="assets/logos/cmufa_logo.png">
+					</div>
+				</a>
+				<a href="http://www.brightsidedental.com/">
+					<div class="small-12">
+						<img src="assets/logos/brightsidedental.png">
+					</div>
+				</a>
+				<a href="http://www.detroitpubliclibrary.org/">
+					<div class="small-12">
+				    	<img src="assets/logos/dpl.png">
+					</div>
+				</a>
+				<a href="http://www.mcc.edu/">
+					<div class="small-12">
+						<img src="assets/logos/mottcc.png">
+					</div>
+				</a>
+				<a href="http://www.nthconsultants.com/">
+					<div class="small-12">
+						<img src="assets/logos/nth_consultants.png">
+					</div>
+				</a>
+				<a href="http://www.masco.com">
+					<div class="small-12">
+						<img src="assets/logos/masco.png">
+					</div>
+				</a>
 			</section>
 
 			<br>
@@ -150,7 +170,7 @@
 		</div>
 	</div>
 
-	<div class="row" style="width: 100%; max-width: 100%; border: 3px solid rgb(248, 112, 69); ">
+	<div class="row" id="why-donate-container">
 
 		<br>
 		<br>
@@ -165,13 +185,17 @@
 			<br>
 
 			<div>
-				<p style="margin: 0 auto; max-width: 900px; text-align: left;">Pack Your Back relies on the kind donations from the community and various businesses in order to provide resources for children to succeed in the classroom. Since there are always more children in need, Pack Your Back is always in need of donations to continue providing students with resources.</p>
+				<p style="margin: 0 auto; max-width: 900px; text-align: left;">
+					Pack Your Back relies on the kind donations from the community and various businesses in order to provide resources for children to succeed in the classroom. Since there are always more children in need, Pack Your Back is always in need of donations to continue providing students with resources.
+				</p>
 			</div>
 
 			<br>
 			<br>
 
-			<p style="margin: 0 auto; max-width: 900px; text-align: left;">In addition to donations of backpacks and other school supplies, the Pack Your Back movement is relying on sponsorships from local businesses and organizations to fund the cause. Click the link below for more information.</p>
+			<p style="margin: 0 auto; max-width: 900px; text-align: left;">
+				In addition to donations of backpacks and other school supplies, the Pack Your Back movement is relying on sponsorships from local businesses and organizations to fund the cause. Click the link below for more information.
+			</p>
 
 			<br>
 			<br>
@@ -180,16 +204,19 @@
 			<br>
 
 			<div class="columns small-6" style="text-align:center;">
-				<a href="https://www.classy.org/campaign/Pack-Your-Back/c128367"><button>Donate Now</button></a>
+				<a href="https://www.classy.org/campaign/Pack-Your-Back/c128367">
+					<button>Donate Now</button>
+				</a>
 			</div>
 			<div class="columns small-6" style="text-align:center;">
-				<a href="https://www.classy.org/campaign/Pack-Your-Back/c128367"><button>More Info</button></a>
+				<a href="https://www.classy.org/campaign/Pack-Your-Back/c128367">
+					<button>More Info</button>
+				</a>
 			</div>
 
 		</div>
 
 		<div class="columns small-12 medium-6 large-4">
-
 			<div class="columns small-12">
 
 				<img src="assets/index_pics/kids.png" style="width:494px; height:240px; position:relative; border: 1px solid rgb(55,55,55);">
@@ -217,7 +244,9 @@
 		<br>
 
 		<div class="index-quote-self" style="text-align:left; margin: 0 auto;">
-			<h4 style="color: rgb(242,242,246); text-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black;" class="index-quote-h4">23% of Michigan residents fall below the poverty line.  There is a constant need for education supplies to help children succeed.</h4>
+			<h4 style="color: rgb(242,242,246); text-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black;" class="index-quote-h4">
+				23% of Michigan residents fall below the poverty line.  There is a constant need for education supplies to help children succeed.
+			</h4>
 		</div>
 
 		<br>
@@ -239,7 +268,9 @@
 			<br style="margin-top: 1px;">
 
 			<h2>Our Needs</h2>
-			<p class="story_p" style="text-align: left; margin: 0 auto;">Pack Your Back relies on the charitable donations from the community members to help supply students with resources for the classroom.</p>
+			<p class="story_p" style="text-align: left; margin: 0 auto;">
+				Pack Your Back relies on the charitable donations from the community members to help supply students with resources for the classroom.
+			</p>
 
 			<br>
 			<br>
@@ -262,7 +293,9 @@
 			<br>
 
 			<h2>Our Leaders</h2>
-			<p class="story_p" style="text-align: left; margin: 0 auto;">Pack Your Back is proud to be led by a young generation, and we believe it's our responsibility to build a system for helping younger generations receive the education they need.</p>
+			<p class="story_p" style="text-align: left; margin: 0 auto;">
+				Pack Your Back is proud to be led by a young generation, and we believe it's our responsibility to build a system for helping younger generations receive the education they need.
+			</p>
 
 			<br>
 			<br>
@@ -283,12 +316,14 @@
 			<br>
 
 			<h2>Our Story</h2>
-			<p class="story_p" style="text-align: left; margin: 0 auto;">Working for the benefit of the underprivileged in the public education system through distribution events and community get-togethers, helping to give students opportunities to interact and grow.</p>
+			<p class="story_p" style="text-align: left; margin: 0 auto;">
+				Working for the benefit of the underprivileged in the public education system through distribution events and community get-togethers, helping to give students opportunities to interact and grow.
+			</p>
 
 			<br>
 			<br>
 			<br>
-			
+
 		</div>
 	</div>
 
