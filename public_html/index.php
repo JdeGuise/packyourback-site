289f8ecab777c97b356d9857f8cd6303b580a806
John deGuise <jrd120893@gmail.com>
2018-09-14 09:14:58 -0600

    Refactoring css out of html doc; prepping br tags for removal

--- public_html/index.php
+++ public_html/index.php
@@ -10,105 +10,117 @@
 	<link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico" />
 	<link rel="stylesheet" type="text/css" href="slick-1.6.0/slick/slick.css"/>
 	<link rel="stylesheet" type="text/css" href="slick-1.6.0/slick/slick-theme.css">
+
 	<script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
 </head>
 
 <body>
 	<?php include('html/includes/header.php'); ?>
+
 	<br>
 	<br>
 	<br>
 
 	<div class="row">
-		<div class="columns small-12 medium-12 large-12" style="background-color:rgb(250,250,252); border: 3px solid rgb(248,152,72); margin-top: 3px;">
-			<a href="https://www.paypal.me/PackYourBack"><img src="assets/index_pics/donatebanner.png" class="mySlides fade jumboimg" style="width: auto;"></a>
-			<a href="#" id="distribution_slide"><img src="assets/index_pics/2.png" class="mySlides fade jumboimg"></a>
-			<a href="html/leadership.php"><img src="assets/index_pics/3.png" class="mySlides fade jumboimg"></a>
+		<div class="columns small-12 medium-12 large-12" id="jumbotron">
+			<a href="https://www.paypal.me/PackYourBack">
+				<img class="mySlides fade jumboimg" src="assets/index_pics/donatebanner.png">
+			</a>
+			<a href="#" id="distribution_slide">
+				<img class="mySlides fade jumboimg" src="assets/index_pics/2.png">
+			</a>
+			<a href="html/leadership.php">
+				<img class="mySlides fade jumboimg" src="assets/index_pics/3.png">
+			</a>
 		</div>
 	</div>
 
-	<div class="row" style="top: 50px; width:100%; border: 3px solid rgb(22,22,148);" id="distribution_section">
-		<br>
-		<br>
-		<br>
-		<h1 style="text-align: center;"><b><u>Upcoming School Distributions</u></b></h1>
-		<br>
-		<br>
-<!-- 		<div class="columns small-12 medium-12 large-6" style="text-align: center;">
+	<div class="row distribution_section">
 
-			<br>
-			<br>
-				<iframe class="distribution_map1" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2917.0214024582697!2d-83.67468904849547!3d43.01993667904583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8823818c8849e1e9%3A0xcfc2b7565e13459a!2sMott+Community+College!5e0!3m2!1sen!2sus!4v1498780899121" frameborder="0" style="border: 1px solid rgb(55,55,55); bottom: 45px;position: relative;" allowfullscreen></iframe>
-		</div>
-		<div class="columns small-12 medium-12 large-6" style="position: relative; left: 20px;">
-			<h3 style="text-align: left;">Mott Community College - August 6th, 2017</h3>
-			<h5 style="text-align: left;">Flint, Michigan</h5>
-			<br>
-			<br>
-			<div class="columns small-12 medium-12 large-12" style="text-align: center;">
-				<p style="text-align:left; max-width: 800px;">Pack Your Back is proud to be back in Flint, Michigan again this year for another school supply distribution. After providing school supplies to over 500 students in 2016, we are ready to do it again. Our 2017 distribution will take place at the Mott Community College Event Center on August 6th from 2:00-5:00pm. We are proud to partner with the Feminist League organization and Mott Community College to make this distribution possible.</p>
-
-				<br>
-				<br>
-				<br>
-
-				<a href="html/fps.php"><button style="bottom: 60px;">Read More</button></a>
-			</div>
-		</div> -->
+		<br>
+		<br>
+		<br>
+		
+		<h1>
+			<b><u>Upcoming School Distributions</u></b>
+		</h1>
+		
+		<br>
+		<br>
 
 		<hr class="hrstyle">
+
 		<br>
 		<br>
 
-
-		<div class="columns small-12 medium-12 large-6" style="text-align: center;left: 20px;">
-			<h3 style="text-align: left;">Detroit Public Library - August 12th, 2017</h3>
-			<h5 style="text-align: left;">Detroit, Michigan</h5>
+		<div class="columns small-12 medium-12 large-6">
+			<h3>Detroit Public Library - August 12th, 2017</h3>
+			<h5>Detroit, Michigan</h5>
+			
 			<br>
 
-			<div class="columns small-12 medium-12 large-12" style="text-align: center;">
+			<div class="columns small-12 medium-12 large-12">
+
 				<br>
-				<p style="text-align: left; max-width: 800px;">Pack Your Back is proud to be in Detroit, Michigan this year for a school supply distribution. After providing school supplies to over 500 students in Flint, Michigan, we are ready to do it again. Our 2017 Detroit distribution will take place at the Detroit Public Library – Main Branch on August 12th from 1:00-5:00pm. We are proud to partner with the March for Public Education and the Detroit Public Library to make this distribution possible.</p>
+
+				<p>Pack Your Back is proud to be in Detroit, Michigan this year for a school supply distribution. After providing school supplies to over 500 students in Flint, Michigan, we are ready to do it again. Our 2017 Detroit distribution will take place at the Detroit Public Library – Main Branch on August 12th from 1:00-5:00pm. We are proud to partner with the March for Public Education and the Detroit Public Library to make this distribution possible.</p>
 
 				<br>
 				<br>
 				<br>
 
-				<a href="html/dps.php"><button style="bottom: 60px;">Read More</button></a>
+				<a href="html/dps.php"><button>Read More</button></a>
 			</div>
 		</div>
-		<div class="columns small-12 medium-12 large-6" style="text-align: center;">
+		<div class="columns small-12 medium-12 large-6">
 
 			<br>
 			<br>
-				<iframe class="distribution_map2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2948.248518823795!2d-83.06880814851169!3d42.35854267908491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824d2bcfe73c635%3A0xc56df4d96dd2790a!2s5201+Woodward+Ave%2C+Detroit%2C+MI+48202!5e0!3m2!1sen!2sus!4v1498781104910" frameborder="0" style="border: 1px solid rgb(55,55,55); bottom: 45px; position: relative;text-align: center;" allowfullscreen></iframe>
+
+			<iframe class="distribution_map2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2948.248518823795!2d-83.06880814851169!3d42.35854267908491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824d2bcfe73c635%3A0xc56df4d96dd2790a!2s5201+Woodward+Ave%2C+Detroit%2C+MI+48202!5e0!3m2!1sen!2sus!4v1498781104910" frameborder="0" style="border: 1px solid rgb(55,55,55); bottom: 45px; position: relative;text-align: center;" allowfullscreen></iframe>
 		</div>
 		<hr class="hrstyle">
 		<div class="columns small-12 medium-6 large-6">
+			
 			<br>
+			
 			<h3 style="text-align: center;">Little Miss Flint</h3>
+			
 			<br>
+			
 			<p style="padding-left: 35px; position: relative; text-align: left;">Pack Your Back would like to thank Mari Copeny, also known as "Little Miss Flint” for assisting our organization in raising funds to purchase over 1,000 for students in Flint, Michigan. "We need to help kids in Flint that can't afford a backpack or school supplies so the people can go to school," she said. "School is important. Education is life.” - Mari Copeny</p>
+			
 			<br>
+			
 			<p style="left: 35px; position: relative; text-align: left;">To donate to Mari’s 2018 Pack Your Back campaign, please visit her fundraising page, located <a href="https://www.classy.org/team/129320">here</a>.</p>
+			
 			<br>
+
 		</div>
 		<div class="columns small-12 medium-6 large-6" style="text-align:center;">
 			<img src="assets/index_pics/lmf.png" width="80%" height="80%" style="border: 1px solid black;">
+			
 			<br>
 			<br>
+
 		</div>
+
 		<br>
 		<br>
+
 	</div>
 
 	<div class="row" style="border: 3px solid rgb(111,111,111); margin: 0 auto;">
 		<div class="columns small-12 medium-12 large-12" style="margin-bottom: 15px; width: 100%;">
+
 			<br>
 			<br>
+
 			<h4 style="text-align: center;"><b>Our sincerest thanks to our 2017/2018 organizational sponsors.</h4>
+
 			<br>
 			<br>
+
 			<section class="sponsorship_container center slider" style="width: 85%; margin: 0 auto;">
 				<a href="https://www.concentra.com/"><div class="small-12" style="width: 100%;">
 					<img src="assets/logos/concentra.png" style="width: auto;">
@@ -132,31 +144,41 @@
 					<img src="assets/logos/masco.png" style="width: 95%;">
 				</div></a>
 			</section>
+
 			<br>
+
 		</div>
 	</div>
 
 	<div class="row" style="width: 100%; max-width: 100%; border: 3px solid rgb(248, 112, 69); ">
+
 		<br>
 		<br>
+
 		<div class="columns small-12 medium-6 large-8" id="why_donate_content">
 
 			<br>
+
 			<h1 style="text-align: center;">Why Donate?</h1>
+
 			<br>
 			<br>
+
 			<div>
 				<p style="margin: 0 auto; max-width: 900px; text-align: left;">Pack Your Back relies on the kind donations from the community and various businesses in order to provide resources for children to succeed in the classroom. Since there are always more children in need, Pack Your Back is always in need of donations to continue providing students with resources.</p>
 			</div>
 
 			<br>
 			<br>
+
 			<p style="margin: 0 auto; max-width: 900px; text-align: left;">In addition to donations of backpacks and other school supplies, the Pack Your Back movement is relying on sponsorships from local businesses and organizations to fund the cause. Click the link below for more information.</p>
+
 			<br>
 			<br>
 
 			<br>
 			<br>
+
 			<div class="columns small-6" style="text-align:center;">
 				<a href="https://www.classy.org/campaign/Pack-Your-Back/c128367"><button>Donate Now</button></a>
 			</div>
@@ -171,16 +193,21 @@
 			<div class="columns small-12">
 
 				<img src="assets/index_pics/kids.png" style="width:494px; height:240px; position:relative; border: 1px solid rgb(55,55,55);">
+
 				<br>
+
 			</div>
 
 			<div class="columns small-12">
 
 				<br>
+
 				<img src="assets/index_pics/1.jpeg" style="height:240px; position: relative; border: 1px solid rgb(55,55,55);">
+
 				<br>
 				<br>
 				<br>
+
 			</div>
 		</div>
 	</div>
@@ -188,161 +215,91 @@
 	<div class="row" class="index-quote-wrapper" style="margin: 0 auto; background-color: rgb(0,0,0); ">
 
 		<br>
+
 		<div class="index-quote-self" style="text-align:left; margin: 0 auto;">
 			<h4 style="color: rgb(242,242,246); text-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black;" class="index-quote-h4">23% of Michigan residents fall below the poverty line.  There is a constant need for education supplies to help children succeed.</h4>
 		</div>
+
 		<br>
 
 	</div>
 	<div class="story_circle_container row" style="border: 4px solid rgb(44,84,165); text-align: center; height: auto;">
 
 		<div class="columns small-12 medium-12 large-4 story_feature">
+
 			<br>
 			<br>
+
 			<div class="story_circles">
 				<a href="html/getinvolved.php">
 					<img src='assets/story_pics/supplies.png'>
 				</a>
 			</div>
+
 			<br style="margin-top: 1px;">
+
 			<h2>Our Needs</h2>
 			<p class="story_p" style="text-align: left; margin: 0 auto;">Pack Your Back relies on the charitable donations from the community members to help supply students with resources for the classroom.</p>
+
 			<br>
 			<br>
 			<br>
 			<br>
+
 		</div>
 
 		<div class="columns small-12 medium-12 large-4 story_feature">
+
 			<br>
 			<br>
+
 			<div class="story_circles">
 				<a href="html/leadership.php">
 					<img src='assets/story_pics/swinging.png'>
 				</a>
 			</div>
+
 			<br>
+
 			<h2>Our Leaders</h2>
 			<p class="story_p" style="text-align: left; margin: 0 auto;">Pack Your Back is proud to be led by a young generation, and we believe it's our responsibility to build a system for helping younger generations receive the education they need.</p>
+
 			<br>
 			<br>
 			<br>
+
 		</div>
 		<div class="columns small-12 medium-12 large-4 story_feature">
+
 			<br>
 			<br>
+
 			<div class="story_circles">
 				<a href="https://www.cmich.edu/stamp/Pages/pack-your-back.aspx">
 					<img src='assets/story_pics/paints.png'>
 				</a>
 			</div>
+
 			<br>
+
 			<h2>Our Story</h2>
 			<p class="story_p" style="text-align: left; margin: 0 auto;">Working for the benefit of the underprivileged in the public education system through distribution events and community get-togethers, helping to give students opportunities to interact and grow.</p>
+
 			<br>
 			<br>
 			<br>
+			
 		</div>
 	</div>
 
-	<div class="row" style="border: 4px solid rgb(250,130,30);">
-		<br>
-		<br>
-		<h3 style="text-align: center;">Keep up with Pack Your Back</h3>
-		<br>
-		<br>
+	<?php include("./html/includes/socialmedia.php") ?>
+	<?php include("./html/includes/footer.php") ?>
 
-		<div class="columns small-6 medium-3 large-3">
-			<div class="sm_circles">
-				<a href="html/contact.php">
-					<img class="social_media_icons" src="assets/social_media/email-icon.png">
-					<br>
-					<h6 class="social_media_text">Send us an<br>Email</h6>
-				</a>
-			</div>
-			<br>
-			<br>
-			<br>
-		</div>
-
-		<div class="columns small-6 medium-3 large-3">
-			<div class="sm_circles">
-				<a href="https://twitter.com/PackYourBack">
-					<img class="social_media_icons" src="assets/social_media/circle-twitter-128.png">
-					<br>
-					<h6 class="social_media_text">Follow us on Twitter</h6>
-				</a>
-			</div>
-			<br>
-			<br>
-			<br>
-		</div>
-
-		<div class="columns small-6 medium-3 large-3">
-			<div class="sm_circles">
-				<a href="https://www.facebook.com/PackYourBack">
-					<img class="social_media_icons" src="assets/social_media/circle-facebook-128.png">
-					<br>
-					<h6 class="social_media_text">Like us on Facebook</h6>
-				</a>
-			</div>
-			<br>
-			<br>
-			<br>
-		</div>
-
-		<div class="columns small-6 medium-3 large-3">
-			<div class="sm_circles">
-				<a href="https://www.linkedin.com/company/pack-your-back">
-					<img class="social_media_icons" src="assets/social_media/circle-linkedin-128.png">
-					<br>
-					<h6 class="social_media_text">Follow us on LinkedIn</h6>
-				</a>
-			</div>
-			<br>
-			<br>
-			<br>
-		</div>
-	</div>
-	<!-- Footer Section -->
-
-	<?php include("html/includes/footer.php") ?>
-
-<script>
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(sIndex) {
-	var i;
-	var mySlides = document.getElementsByClassName("mySlides");
-
-	for (i = 0; i < mySlides.length; i++) {
-		mySlides[i].style.display = "none";
-	}
-	slideIndex++;
-
-	if (slideIndex > mySlides.length) {
-		slideIndex = 1;
-	}
-
-	mySlides[slideIndex-1].style.display = "block";
-
-	setTimeout(showDivs, 5000);
-}
-
-var slideIndex = 0;
-
-showDivs(slideIndex);
-
-</script>
-
-<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
-
-	<script type="text/javascript" src="js/scrollScript.js"></script>
-	<script type="text/javascript" src="js/menu.js" ></script>
-	<script type="text/javascript" src="js/socialMediaAnimation.js"></script>
-	<script type="text/javascript" src="js/slickScript.js"></script>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+	<script type="text/javascript" src="./js/slideshowScript.js"></script>
+	<script type="text/javascript" src="./js/scrollScript.js"></script>
+	<script type="text/javascript" src="./js/menu.js" ></script>
+	<script type="text/javascript" src="./js/socialMediaAnimation.js"></script>
+	<script type="text/javascript" src="./js/slickScript.js"></script>
 	<script type="text/javascript" src="./slick-1.6.0/slick/slick.js" charset="utf-8"></script>
 </body>
