6f0197872228a60421aa1c8d95aab75452192e1f
John deGuise <jrd120893@gmail.com>
2018-09-16 12:55:28 -0600

    continued refactoring of menu.js

--- public_html/js/menu.js
+++ public_html/js/menu.js
@@ -1,50 +1,49 @@
-$(document).ready( function() {
-	$('#hamburger').on('click', function(){
-		var dropdown = document.getElementById("myDropdown");
-		console.log(dropdown);
-		var dropdown_overlay = document.getElementById("dropdownOverlay");
-		var display = dropdown.style.display;
+const hamburger = document.getElementById('hamburger');
 
-		if (display == "block"){
-			dropdown.style.display= "none";
-			dropdown_overlay.style.display = "none";
-			$(this).css({"transform" : "rotate(12deg)", "opacity" : ".8"});
-		}
-		else {
-			dropdown.style.display = "block";
-			dropdown_overlay.style.display = "block";
+hamburger.addEventListener('click', function() {
+	const dropdown = document.getElementById("myDropdown");
+	const dropdown_overlay = document.getElementById("dropdownOverlay");
+	const display = dropdown.style.display;
 
-			$(this).css({"transform" : "rotate(-90deg)", "opacity" : ".8"});
-		}
-	});
+	if (display == "block"){
+		dropdown.style.display= "none";
+		dropdown_overlay.style.display = "none";
+		$(this).css({"transform" : "rotate(12deg)", "opacity" : "1"});
+	}
+	else {
+		dropdown.style.display = "block";
+		dropdown_overlay.style.display = "block";
+
+		$(this).css({"transform" : "rotate(-90deg)", "opacity" : "1"});
+	}
 });
 
-$("#hamburger").hover(
-	function(){
-		if (document.getElementById("myDropdown").style.display == "block"){
-			$(this).css({
-				"opacity" : ".8",
-				"transform" : "rotate(-90deg)",
-				"transition" : ".3s ease"
-			});
-		} else {
-			$(this).css({
-				"opacity" : ".8",
-				"transform" : "rotate(12deg)",
-				"transition" : ".3s ease"
-			});
-		}
-	}, function(){
-		if (document.getElementById("myDropdown").style.display == "block"){
-			$(this).css({
-				"opacity" : "1",
-				"transform" : "rotate(-90deg)"
-			});
-		} else {
-			$(this).css({
-				"opacity" : "1",
-				"transform" : "rotate(0deg)"
-			});
-		}
+hamburger.addEventListener('mouseover', function() {
+	if (document.getElementById("myDropdown").style.display == "block"){
+		$(this).css({
+			"opacity" : "1",
+			"transform" : "rotate(-90deg)",
+			"transition" : ".3s ease"
+		});
+	} else {
+		$(this).css({
+			"opacity" : "1",
+			"transform" : "rotate(12deg)",
+			"transition" : ".3s ease"
+		});
 	}
-);
+});
+
+hamburger.addEventListener('mouseout', function() {
+	if (document.getElementById("myDropdown").style.display == "block"){
+		$(this).css({
+			"opacity" : "1",
+			"transform" : "rotate(-90deg)"
+		});
+	} else {
+		$(this).css({
+			"opacity" : "1",
+			"transform" : "rotate(0deg)"
+		});
+	}
+});
