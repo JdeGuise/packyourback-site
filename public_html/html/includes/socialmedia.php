ce67e71c4c2571f589132ead8501796094cceb22
John deGuise <jrd120893@gmail.com>
2018-09-17 14:53:56 -0600

    Updates social media; modularizes social media reference

--- public_html/html/includes/socialmedia.php
+++ public_html/html/includes/socialmedia.php
@@ -6,11 +6,11 @@
 	<br>
 
 	<div class="columns small-6 medium-3 large-3">
-		<div class="sm_circles">
+		<div class="sm-circles">
 			<a href="html/contact.php">
-				<img class="social_media_icons" src="assets/social_media/email-icon.png">
+				<img class="social-media-icons" src="assets/social_media/email-icon.png">
 				<br>
-				<h6 class="social_media_text">Send us an<br>Email</h6>
+				<h6 class="social-media-text">Send us an<br>Email</h6>
 			</a>
 		</div>
 		<br>
@@ -19,11 +19,11 @@
 	</div>
 
 	<div class="columns small-6 medium-3 large-3">
-		<div class="sm_circles">
+		<div class="sm-circles">
 			<a href="https://twitter.com/PackYourBack">
-				<img class="social_media_icons" src="assets/social_media/circle-twitter-128.png">
+				<img class="social-media-icons" src="assets/social_media/circle-twitter-128.png">
 				<br>
-				<h6 class="social_media_text">Follow us on Twitter</h6>
+				<h6 class="social-media-text">Follow us on Twitter</h6>
 			</a>
 		</div>
 		<br>
@@ -32,11 +32,11 @@
 	</div>
 
 	<div class="columns small-6 medium-3 large-3">
-		<div class="sm_circles">
+		<div class="sm-circles">
 			<a href="https://www.facebook.com/PackYourBack">
-				<img class="social_media_icons" src="assets/social_media/circle-facebook-128.png">
+				<img class="social-media-icons" src="assets/social_media/circle-facebook-128.png">
 				<br>
-				<h6 class="social_media_text">Like us on Facebook</h6>
+				<h6 class="social-media-text">Like us on Facebook</h6>
 			</a>
 		</div>
 		<br>
@@ -45,11 +45,11 @@
 	</div>
 
 	<div class="columns small-6 medium-3 large-3">
-		<div class="sm_circles">
+		<div class="sm-circles">
 			<a href="https://www.linkedin.com/company/pack-your-back">
-				<img class="social_media_icons" src="assets/social_media/circle-linkedin-128.png">
+				<img class="social-media-icons" src="assets/social_media/circle-linkedin-128.png">
 				<br>
-				<h6 class="social_media_text">Follow us on LinkedIn</h6>
+				<h6 class="social-media-text">Follow us on LinkedIn</h6>
 			</a>
 		</div>
 		<br>
