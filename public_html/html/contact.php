16b973d11f217d5ba6f6ad8e257d3c415fc87cbb
John deGuise <jrd120893@gmail.com>
2018-09-17 16:37:53 -0600

    Fixes #77; refactors inline css into external doc

--- public_html/html/contact.php
+++ public_html/html/contact.php
@@ -1,7 +1,7 @@
 <title>Contact Us - Pack Your Back</title>
 <?php include('includes/head.php'); ?>
 <body class="body">
-		<?php include('includes/htmlheader.php'); ?>
+	<?php include('includes/header.php'); ?>
 
 	<br>
 	<br>
@@ -9,39 +9,35 @@
 
 	<div class="columns small-12 medium-12 large-12">
 
-	<br>
-	<br>
+		<br>
+		<br>
 
-		<div class="login_wrapper" style="background-color: lightgray; margin:0 auto; max-width: 600px; min-height: 200px;">
-
-		  <form method="post">
-			  <div style="margin-left: 10px; margin-right: 10px;">
+		<div id="contact-wrapper">
+		  <form method="post" id="contact-form">
+			  <div>
 				  <label for="name">Name:</label>
 				  <input type="text" id="name" name="user_name" />
 			  </div>
-			  <div style="margin-left: 10px; margin-right: 10px;">
+			  <div>
 				  <label for="mail">E-mail:</label>
 				  <input type="email" id="mail" name="user_mail" />
 			  </div>
-			  <div style="margin-left: 10px; margin-right: 10px;">
+			  <div>
 				  <label for="msg">Message:</label>
 				  <textarea id="msg" name="user_message"></textarea>
 			  </div>
 
-			  <div class="button" style="left: 20px; width:180px; height:80px;">
+			  <div class="button">
 				  <button type="submit">Submit</button>
 			  </div>
 		  </form>
-
 		</div>
+	</div>
 
-	</div
   <br>
   <br>
-    <? include("includes/footer.php");?>
 
-	</body>
+	<? include("includes/footer.php");?>
 	<script src="../js/menu.js" ></script>
-
-
-	</html>
+</body>
+</html>
