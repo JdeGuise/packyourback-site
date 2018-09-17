3b0a8417bd3acb307fdc8a1ba73849651daa78ee
John deGuise <jrd120893@gmail.com>
2018-09-17 16:26:32 -0600

    Refactors CSS into external stylesheet

--- public_html/html/getinvolved.php
+++ public_html/html/getinvolved.php
@@ -7,46 +7,55 @@
 	<br>
 	<br>
 	<br>
+	
 	<div class="row">
 		<hr class="hrstyle">
-		<div class="columns small-12">
+		<div class="columns small-12" id="get-involved-prompt-container">
 
-			<h5 style="text-align:center;">Helping raise resources for those in need, Pack Your Back intends to make a difference one student at a time.  Here's how you can help.</h5>
+			<h5>Helping raise resources for those in need, Pack Your Back intends to make a difference one student at a time.  Here's how you can help.</h5>
 
 			<br>
+
 		</div>
 
 		<hr class="hrstyle">
+
 		<br>
+
 	</div>
 
 	<div class="row">
 
-		<div class="columns small-12 medium-12 large-12" style="border: 4px solid rgb(43,89,173);">
+		<div class="columns small-12 medium-12 large-12" id="get-involved-container">
 
 			<br>
-			<h2 style="text-align:center;">Opportunities</h2>
+
+			<h2>Opportunities</h2>
 
 			<br>
 			<br>
 
-			<div style="margin: 0 auto; max-width: 1100px;">
+			<div id="get-involved-explanation">
 				<p>We are currently accepting applications for membership and volunteer positions. We accept applicants on a rolling basis.  All membership and volunteer applicants will be contacted after submitting their application.  New applicants will be interviewed before being accepted.</p>
-			<p>Volunteers must be at least 16 years of age upon the time of becoming a member or a volunteer.</p>
-			<p>Questions? Please contact info@packyourback.org or contact us at (877) 932-5277.</p>
+				<p>Volunteers must be at least 16 years of age upon the time of becoming a member or a volunteer.</p>
+				<p>Questions? Please contact info@packyourback.org or contact us at (877) 932-5277.</p>
 			</div>
 
 			<br>
 
-			<div style="text-align: center">
-				<button onclick="location.href='https://www.cognitoforms.com/PackYourBack/VolunteerSignup2'">Sign up to volunteer</button>
+			<div id="get-involved-link">
+				<button onclick="location.href='https://www.cognitoforms.com/PackYourBack/VolunteerSignup2'">
+					Sign up to volunteer
+				</button>
 			</div>
+
 			<br>
+
 		</div>
 	</div>
 
 	<?php include("../html/includes/socialmedia.php"); ?>
-	<?php include("includes/htmlfooter.php"); ?>
+	<?php include("includes/footer.php"); ?>
 
 	<script src="../js/menu.js" ></script>
 	<script src="../js/socialMediaAnimation.js"></script>
