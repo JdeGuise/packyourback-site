81db3e74bf9b0377a6d2971a7f0ca4fa7a0c6cc2
John deGuise <jrd120893@gmail.com>
2018-09-17 15:42:40 -0600

    Updates head resource references

--- public_html/html/includes/head.php
+++ public_html/html/includes/head.php
@@ -4,11 +4,11 @@
 	<meta charset="utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-	<link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico"/>
-	<link rel="stylesheet" type="text/css" href="css/style.css"/>
-	<link rel="stylesheet" type="text/css" href="css/foundation.css" />
-	<link rel="stylesheet" type="text/css" href="css/w3.css"/>
-	<link rel="stylesheet" type="text/css" href="slick-1.6.0/slick/slick.css"/>
-	<link rel="stylesheet" type="text/css" href="slick-1.6.0/slick/slick-theme.css"/>
+	<link rel="shortcut icon" type="image/x-icon" href="../assets/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="../css/style.css"/>
+	<link rel="stylesheet" type="text/css" href="../css/foundation.css" />
+	<link rel="stylesheet" type="text/css" href="../css/w3.css"/>
+	<link rel="stylesheet" type="text/css" href="../slick-1.6.0/slick/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="../slick-1.6.0/slick/slick-theme.css"/>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Raleway|Roboto|Source+Sans+Pro"/>
 </head>
