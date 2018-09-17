dc3924387f2c3016f1be5dee729900f5cb880fe0
John deGuise <jrd120893@gmail.com>
2018-09-17 14:49:46 -0600

    Refactors head into external file; refactors footer css into external file

---
+++ public_html/html/includes/head.php
@@ -0,0 +1,14 @@
+<?php ?>
+<head>
+	<title>Pack Your Back - Supplying Success</title>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+	<link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<link rel="stylesheet" type="text/css" href="css/foundation.css" />
+	<link rel="stylesheet" type="text/css" href="css/w3.css"/>
+	<link rel="stylesheet" type="text/css" href="slick-1.6.0/slick/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="slick-1.6.0/slick/slick-theme.css"/>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Raleway|Roboto|Source+Sans+Pro"/>
+</head>
